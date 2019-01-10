c070df8c17d98e886c81a40c0df0774e1bcba184
Alexander Zdorov <a.zdorov@pixonic.ru>
2019-01-10 22:20:02 +0300

    Add files via upload
    тест на сумму сторон всех типов треугольников

---
+++ Solution1/ConsoleApp1/nUnitTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using ConsoleApp1;
+
+class Tests
+{
+    RegularTriangle regularTriangle = new RegularTriangle();
+    EqualSidesTriangle equalSidesTriangle = new EqualSidesTriangle();
+    RectangularTriangle rectangularTriangle = new RectangularTriangle();
+
+    [Test]
+    public void Angles_Sum_Test_RegularTriangle()
+    {
+        Assert.That(MathActions.AnglesSum(regularTriangle.AngleA, regularTriangle.AngleB, regularTriangle.AngleC), Is.EqualTo(180));
+    }
+    [Test]
+    public void Angles_Sum_Test_EqualSidesTriangle()
+    {
+        Assert.That(MathActions.AnglesSum(equalSidesTriangle.AngleA, equalSidesTriangle.AngleB, equalSidesTriangle.AngleC), Is.EqualTo(180));
+    }
+    [Test]
+    public void Angles_Sum_Test_RectangularTriangle()
+    {
+        Assert.That(MathActions.AnglesSum(rectangularTriangle.AngleA, rectangularTriangle.AngleB, rectangularTriangle.AngleC), Is.EqualTo(180));
+    }
+}
